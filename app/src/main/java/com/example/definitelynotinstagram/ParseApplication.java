7f23c2e5c60f884281681a2dfd4d4eb8971da52a
Sheng Dong <tomdong517@gmail.com>
2021-03-12 10:55:22 -0500

    Completed all Stretch Stories, will work on improving the general 'feel' of things.

--- app/src/main/java/com/example/definitelynotinstagram/ParseApplication.java
+++ app/src/main/java/com/example/definitelynotinstagram/ParseApplication.java
@@ -3,12 +3,17 @@ package com.example.definitelynotinstagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
+
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("061HW9gusGTVnOg9FYM79DahypCmpT6dkDxInrTc")
                 .clientKey("PHJYm8rMLY0wIcRh5ocqD4sAc2JpCmmmkE4cENAF")
